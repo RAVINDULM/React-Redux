1a9ef2bb494ea2ac2141ffb0a59d5bf0149a1085
RAVINDULM <ravindu.kp@gmail.com>
2023-02-24 07:15:05 +0530

    Add components and routing with some issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
-
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductListing from "./components/ProductListing";
+// import ProductDetails from "./components/ProductDetails";
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            {/* <Header /> */}
+            <Route path="product" element={<ProductListing />} />
+            <Route>404 NOT FOUND!</Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
